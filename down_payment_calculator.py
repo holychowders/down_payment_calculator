6ada299facdc2d40adf5cc3f17c1cfb8ed670cc9
holychowders <austinxaa@gmail.com>
2020-06-28 15:54:47 -0700

    Major refactor

--- down_payment_calculator.py
+++ down_payment_calculator.py
@@ -1,46 +1,95 @@
-# -*- coding: utf-8 -*-
 """
-Author: jsashbeck (https://github.com/jsashbeck/)
-
-Name: Months to Save for Down Payment on Dream Home
-
-Description: Calculates the number of months required to save in order to afford the down payment on user's dream home, given user's input on the home cost, down payment, interest rate, salary, and amount to save.
+Original Author: https://github.com/jsashbeck
+Refactor Author: https://github.com/holychowders
 """
-print('Name: Months to Save for Down Payment on Dream Home.\n')
 
-# Check whether the following user inputs are real number values.
-try:
-	# Take user inputs.
-	home_cost = float(input('Enter cost of home: '))
-	home_down_payment_percent = float(input('Enter down payment percentage (ie, 0.25 for 25%): '))
-	annual_interest_rate = float(input('Enter interest rate (ie, 0.05 for 5%): '))
-	annual_salary = float(input('Enter your salary: '))
-	annual_salary_saved_percent = float(input('Enter percentage of salary saved (ie, 0.25 for 25%): '))
-except ValueError:
-	raise ValueError('Inputs must be real number values.')
+from time import sleep
+
+def run_down_payment_calculator():
+    """
+    Calculate number of years/months of saving required to afford down payment.
+    """
+
+    success = False
+    print('\nCalculate the Time Required to Save In Order to Afford Down '\
+        'Payment on Dream House.\n\n'
+    )
+
+    try:
+        house_cost = float(input('Enter cost of house: '))
+        assert house_cost > 0, (
+            'The cost of your house must be greater than 0'
+        )
+
+        down_payment_percent = (float(input(
+            'Enter down payment percentage (ie, 25.2 for 25.2%): ')) / 100
+        )
+        assert 0 < down_payment_percent <= 1, (
+            'Down payment percentage must range between 0 and 100'
+        )
+
+        savings_dollars = float(input("Enter reserved savings: "))
+        assert savings_dollars >= 0, 'Reserved savings must not be negative'
+
+        annual_salary = float(input('Enter your salary: '))
+        assert annual_salary > 0, 'Salary must be greater than 0'
+
+        annual_salary_saved_percent = (float(input(
+            'Enter percentage of salary saved (ie, 25 for 25%): ')) / 100
+        )
+        assert 0 < annual_salary_saved_percent <= 1, (
+            'Percentage salary saved must range between 0 and 100'
+        )
+
+        annual_interest_percent = float(input('Enter interest rate: ')) / 100
+        assert 0 <= annual_interest_percent <= 1, (
+            'Annual interest rate must range between 0 and 100'
+        )
+
+    except ValueError:
+        print('\nValueError: You must enter only real number values.\a')
+        return
+    except AssertionError as ae:
+        print(f'\nAssertionError: {ae}\a')
+        return
+    except Exception:
+        print('\nUh oh. An unknown error occured!\a')
+        return
 
 
-# Check whether user inputs are impossible for calculation. Else, proceed normally.
-assert home_cost >= 0, 'The cost of your home must have a non-negative value.'
-assert 0 <= home_down_payment_percent <= 1, 'Home down payment percentage must range between 0 and 1.'
-assert 0 <= annual_interest_rate <= 1, 'Annual interest rate must range between 0 and 1.'
-assert annual_salary >= 0, 'Cannot have a negative salary.'
-assert 0 <= annual_salary_saved_percent <= 1, 'Percentage salary saved must range between 0 and 1.'
+    else:
+        months = 0
+        monthly_salary = annual_salary / 12
+        monthly_interest_percent = annual_interest_percent / 12
 
-if annual_salary == 0 or annual_salary_saved_percent == 0:
-	print('\n\nResult:\n\tIt is not possible to save up for the down payment. Salary is $0, or no portion of salary is saved.')
-else:
-	current_savings = 0
-	months = 0
-	monthly_salary = annual_salary / 12
-	monthly_interest_rate = annual_interest_rate / 12
+        down_payment_dollars = down_payment_percent * house_cost
+        monthly_salary_saved_dollars = (
+            annual_salary_saved_percent * monthly_salary)
 
-	# Increment current savings according to interest rates and portion saved until savings meets or exceeds down payment on home.
-	while current_savings < home_down_payment_percent * home_cost and months <= 1200:
-		current_savings = current_savings * (1 + monthly_interest_rate) + annual_salary_saved_percent * monthly_salary
-		months += 1
+        while savings_dollars < down_payment_dollars:
 
-	if months > 1200:
-		raise OverflowError('It will take in excess of 1200 months (100 years) to afford down payment.')
-	else:
-		print(f'\n\nResult:\n\tYou need to save for {months} months to afford down payment.')		
+            savings_dollars = (
+                savings_dollars * (1 + monthly_interest_percent) +
+                    monthly_salary_saved_dollars)
+
+            months += 1
+
+            if months > 1800:
+                print('\nOverflowError: It would take in excess of '\
+                    '150 years of saving in order to afford down payment.\a'
+                )
+                return
+
+        print('\nResult:\nYou would need to save for '\
+            f'{months // 12} years, {months % 12} months'
+        )
+        success = True
+
+    finally:
+        if success == True:
+            print("\n\nThanks for using this calculator!")
+            input('\nPress "Enter" to end: ')
+            print('Goodbye!')
+            sleep(2)
+        else:
+            input("\n\nPress Enter to end the program")
