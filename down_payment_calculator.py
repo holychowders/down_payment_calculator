c6b4aec8e6cb603c6673aa56f22d36761e8b386e
holychowders <austinxaa@gmail.com>
2020-11-19 11:58:44 -0800

    Remove ability to type 'rerun' and 'exit' at any time

--- down_payment_calculator.py
+++ down_payment_calculator.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 """Calculate number of years/months of saving required to afford down payment."""
 
-EXIT_KEYWORD = "exit"
-RESTART_KEYWORD = "rerun"
-
 
 def main():
     print_newline()
     print(23 * "-")
     print("Down Payment Calculator")
     print(23 * "-")
-    print(f"Type '{EXIT_KEYWORD}' or '{RESTART_KEYWORD}' at any time")
-    print_newline()
 
 
     house_cost = get_user_input("Cost of dream house: ", 1)
@@ -45,42 +40,27 @@ def main():
             print("Overflow Error: It would take in excess of "\
                 "150 years of saving in order to afford down payment.\a"
             )
-            get_user_input(f"Type '{RESTART_KEYWORD}' to restart or type '{EXIT_KEYWORD}' to end: ", at_program_end=True)
 
     years_to_save = months_to_save // 12
     remainder_months_to_save = months_to_save % 12
     print_newline(2)
     print(f"Result: {years_to_save} years, {remainder_months_to_save} months")
-    print_newline(2)
-
-
-    get_user_input(f"Type '{RESTART_KEYWORD}' to restart or type '{EXIT_KEYWORD}' to end: ", at_program_end=True)
 
 
 def print_newline(count=1):
     print(count * "\n", end="")
 
 
-def get_user_input(message, lower_limit=None, upper_limit=None, is_percent_as_int=False, at_program_end=False):
+def get_user_input(message, lower_limit=None, upper_limit=None, is_percent_as_int=False):
     """
-    Return converted numerical value from input if not request to exit or restart main.
+    Return converted numerical value from input.
 
     lower_limit/upper_limit are lower/upper numerical limit. upper_limit not accepted alone;
-    Execute requests to restart or exit.
     Print error message when value not in range and recall get_user_input with same arguments.
     """
     print_newline()
     user_input = input(f"{message}")
 
-    user_wants_to_exit = (user_input == EXIT_KEYWORD)
-    user_wants_to_restart = (user_input == RESTART_KEYWORD)
-    if user_wants_to_exit: exit()
-    elif user_wants_to_restart:
-        print_newline(2)
-        main()
-
-    # If at end, accept only the above keywords.
-    if at_program_end: return get_user_input(f"{message}\a", at_program_end=True)
 
     # Attempt conversion and validation.
     try:
