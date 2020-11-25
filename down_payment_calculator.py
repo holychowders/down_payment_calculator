holychowders/down_payment_calculator

commit c6b4aec8e6cb603c6673aa56f22d36761e8b386e

Remove ability to type 'rerun' and 'exit' at any time

down_payment_calculator.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 """Calculate number of years/months of saving required to afford down payment."""
 
-EXIT_KEYWORD = "exit"
-RESTART_KEYWORD = "rerun"
-
 
 def main():
     print_newline()
     print(23 * "-")
     print("Down Payment Calculator")
     print(23 * "-")
-    print(f"Type '{EXIT_KEYWORD}' or '{RESTART_KEYWORD}' at any time")
-    print_newline()
 
 
     house_cost = get_user_input("Cost of dream house: ", 1)
@@ -45,42 +40,27 @@ def main():
             print("Overflow Error: It would take in excess of "\
                 "150 years of saving in order to afford down payment.\a"
             )
-            get_user_input(f"Type '{RESTART_KEYWORD}' to restart or type '{EXIT_KEYWORD}' to end: ", at_program_end=True)
 
     years_to_save = months_to_save // 12
     remainder_months_to_save = months_to_save % 12
     print_newline(2)
     print(f"Result: {years_to_save} years, {remainder_months_to_save} months")
-    print_newline(2)
-
-
-    get_user_input(f"Type '{RESTART_KEYWORD}' to restart or type '{EXIT_KEYWORD}' to end: ", at_program_end=True)
 
 
 def print_newline(count=1):
     print(count * "\n", end="")
 
 
-def get_user_input(message, lower_limit=None, upper_limit=None, is_percent_as_int=False, at_program_end=False):
+def get_user_input(message, lower_limit=None, upper_limit=None, is_percent_as_int=False):
     """
-    Return converted numerical value from input if not request to exit or restart main.
+    Return converted numerical value from input.
 
     lower_limit/upper_limit are lower/upper numerical limit. upper_limit not accepted alone;
-    Execute requests to restart or exit.
     Print error message when value not in range and recall get_user_input with same arguments.
     """
     print_newline()
     user_input = input(f"{message}")
 
-    user_wants_to_exit = (user_input == EXIT_KEYWORD)
-    user_wants_to_restart = (user_input == RESTART_KEYWORD)
-    if user_wants_to_exit: exit()
-    elif user_wants_to_restart:
-        print_newline(2)
-        main()
-
-    # If at end, accept only the above keywords.
-    if at_program_end: return get_user_input(f"{message}\a", at_program_end=True)
 
     # Attempt conversion and validation.
     try:

commit e4de6825dc2a714217358821336fea31481465ed

Fix bug causing overflow error message to loop
Add 'exit()' to overflow check. If 'is_too_long_to_save' is True,
print the error message and exit.

This bug was caused by commit c6b4aec, which removed a
feature allowing the user to type 'exit' or 'rerun' at any time.

This feature was holding open the prompt in the case that the number
of months to save exceeded 1800 during the primary calculation.
In this case, the user could either end or restart the program.

The feature removed, nothing held the prompt open, so the number
of months would continue to be incremented while this overflow
condition was checked each time.

down_payment_calculator.py
@@ -36,6 +36,7 @@ def main():
             print("Overflow Error: It would take in excess of "\
                 "150 years of saving in order to afford down payment.\a"
             )
+            exit()
 
     display_results(months_to_save)
 