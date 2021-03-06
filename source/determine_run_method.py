10dacaed085cb7dae3abdcd6279a794b6508d76f
holychowders <austinxaa@gmail.com>
2021-03-06 15:09:43 -0800

    Remove unnecessary conditional logic.
    Also remove probably unnecessary comment.

--- source/determine_run_method.py
+++ source/determine_run_method.py
@@ -5,15 +5,11 @@ import gui_main
 
 def determine_run_method():
     try:
-        first_cli_argument = sys.argv[1]  # sys.argv[0] would be the name of the program, not the actual first argument.
+        first_cli_argument = sys.argv[1]
     except IndexError:
         first_cli_argument = None
 
-    if not first_cli_argument:
-        return gui_main
-    elif first_cli_argument == 'gui':
-        return gui_main
-    elif first_cli_argument == 'cli':
+    if first_cli_argument == 'cli':
         return cli_main
     else:
         return gui_main
